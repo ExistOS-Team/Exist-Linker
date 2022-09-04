a7f5713efa5204977bf5feeadd3f140dc1ceebb9
Repeerc <repeerc@qq.com>
2022-09-04 23:32:14 +0800

    Full Relocation Function Implemented.

--- main.c
+++ main.c
@@ -49,17 +49,24 @@ uint8_t *buf_rec_table;
 uint8_t *buf_text_rodata;
 uint8_t *buf_data;
 
+uint32_t off_dynsym_exp;
+uint32_t off_dynstr_exp;
+uint32_t off_rel_plt_exp;
+uint32_t num_rel_plt_exp;
+uint32_t off_rel_dyn_exp;
+uint32_t num_rel_dyn_exp;
+uint32_t off_got_exp;
+
 static Elf32_Sym *sys_symtab;
 static const char *sys_symstr;
 static size_t syssym_num;
 
 elf_t elf_sys;
 elf_t elf_app;
+exp_header_t *exp_h;
 
 uint32_t syssym_hash;
 void Usage();
-
-
 
 uint32_t search_sys_sym(char *name)
 {
@@ -120,17 +127,19 @@ int main(int argc, const char *argv[])
     }
     else if (argc == 5)
     {
-        for (int i = 0; i < 7; i++)
+        for (int i = 0; i < 5; i++)
         {
             if (strcmp(argv[i], "--syself") == 0)
             {
                 strcpy(path_syself, argv[i + 1]);
                 i++;
+                vaild++;
             }
             if (strcmp(argv[i], "--exp") == 0)
             {
                 strcpy(path_exp, argv[i + 1]);
                 i++;
+                vaild++;
             }
         }
         if (vaild != 2)
@@ -335,11 +344,13 @@ int main(int argc, const char *argv[])
             if (strcmp(sname, ".got") == 0)
             {
                 got_table = (uint32_t *)elf_getSectionOffset(&elf_app, i);
+                off_got_exp = (uint32_t)got_table - fo_rel;
                 printf("got_table:%08x\n", got_table);
             }
             if (strcmp(sname, ".dynsym") == 0)
             {
                 dynamic_table = (Elf32_Sym *)elf_getSectionOffset(&elf_app, i);
+                off_dynsym_exp = (uint32_t)dynamic_table - fo_text;
                 printf("dynamic_table:%08x, item:%d\n", dynamic_table, elf_getSectionSize(&elf_app, i) / sizeof(Elf32_Sym));
             }
 
@@ -347,6 +358,8 @@ int main(int argc, const char *argv[])
             {
                 rel_plt = (uint32_t *)elf_getSectionOffset(&elf_app, i);
                 rel_plt_num = elf_getSectionSize(&elf_app, i) / sizeof(Elf32_Rel);
+                off_rel_plt_exp = (uint32_t)rel_plt - fo_rel;
+                num_rel_plt_exp = rel_plt_num;
                 printf("rel_plt:%08x, item:%d\n", rel_plt, rel_plt_num);
             }
 
@@ -354,12 +367,15 @@ int main(int argc, const char *argv[])
             {
                 rel_dyn = (uint32_t *)elf_getSectionOffset(&elf_app, i);
                 rel_dyn_num = elf_getSectionSize(&elf_app, i) / sizeof(Elf32_Rel);
+                off_rel_dyn_exp = (uint32_t)rel_dyn - fo_rel;
+                num_rel_dyn_exp = rel_dyn_num;
                 printf("rel_dyn:%08x, item:%d\n", rel_dyn, rel_dyn_num);
             }
 
             if (strcmp(sname, ".dynstr") == 0)
             {
                 dynstr = (const char *)elf_getSectionOffset(&elf_app, i);
+                off_dynstr_exp = (uint32_t)dynstr - fo_text;
                 printf("dynstr:%08x\n", dynstr);
                 dynstr += (uint32_t)buf_appelf;
             }
@@ -424,7 +440,7 @@ int main(int argc, const char *argv[])
                     {
                         *fill_addr = sym->st_value;
 
-                        //printf("GLOB_DAT, at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
+                        // printf("GLOB_DAT, at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
                     }
                 }
             }
@@ -474,7 +490,7 @@ int main(int argc, const char *argv[])
                     {
                         *fill_addr = sym->st_value;
 
-                        //printf("ABS32   , at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
+                        // printf("ABS32   , at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
                     }
                 }
             }
@@ -524,7 +540,7 @@ int main(int argc, const char *argv[])
                     {
                         *fill_addr = sym->st_value;
 
-                        //printf("R_ARM_JUMP_SLOT, at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
+                        // printf("R_ARM_JUMP_SLOT, at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
                     }
                 }
             }
@@ -590,7 +606,7 @@ int main(int argc, const char *argv[])
                     else
                     {
                         *fill_addr = sym->st_value;
-                        //printf("R_ARM_JUMP_SLOT, at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
+                        // printf("R_ARM_JUMP_SLOT, at:%08x, link_addr-app:%08x -> app:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &dynstr[sym->st_name]);
                     }
                 }
             }
@@ -639,6 +655,17 @@ int main(int argc, const char *argv[])
         expHeader->data_sz = sz_data;
         expHeader->text_sz = sz_text;
 
+        expHeader->dynsym_of = expHeader->text_fo + off_dynsym_exp;
+        expHeader->dynstr_of = expHeader->text_fo + off_dynstr_exp;
+
+        expHeader->rel_dyn_of = expHeader->reloc_fo + off_rel_dyn_exp;
+        expHeader->rel_plt_of = expHeader->reloc_fo + off_rel_plt_exp;
+
+        expHeader->num_rel_dyn = num_rel_dyn_exp;
+        expHeader->num_rel_plt = num_rel_plt_exp;
+
+        expHeader->got_of = expHeader->reloc_fo + off_got_exp;
+
         memcpy(&buf_exp[expHeader->reloc_fo], buf_rec_table, sz_rel);
         memcpy(&buf_exp[expHeader->data_fo], buf_data, sz_data);
         memcpy(&buf_exp[expHeader->text_fo], buf_text_rodata, sz_text);
@@ -648,8 +675,294 @@ int main(int argc, const char *argv[])
         fwrite(buf_exp, 1, sz_exp, f_exp);
 
         free(buf_exp);
-    }else{
-        printf("Function Unimplemented.\n");
+    }
+    else
+    {
+
+        f_syself = fopen(path_syself, "rb");
+        if (!f_syself)
+        {
+            fprintf(stderr, "Failed to open syself\n");
+            exit(-1);
+        }
+
+        f_exp = fopen(path_exp, "rb+");
+        if (!f_exp)
+        {
+            fprintf(stderr, "Failed to open exp\n");
+            fclose(f_syself);
+            exit(-1);
+        }
+
+        fseek(f_syself, 0, SEEK_END);
+        sz_syself = ftell(f_syself);
+        fseek(f_syself, 0, SEEK_SET);
+
+        fseek(f_exp, 0, SEEK_END);
+        sz_exp = ftell(f_exp);
+        fseek(f_exp, 0, SEEK_SET);
+
+        printf("sz_exp:%d\n", sz_exp);
+
+        buf_syself = malloc(sz_syself);
+        if (!buf_syself)
+        {
+            fprintf(stderr, "Failed to alloc memory 1\n");
+            ret = -1;
+            goto exit01;
+        }
+
+        buf_exp = malloc(sz_exp);
+        if (!buf_exp)
+        {
+            fprintf(stderr, "Failed to alloc memory 2\n");
+            ret = -1;
+            goto exit01;
+        }
+
+        fread(buf_exp, 1, sz_exp, f_exp);
+
+        fread(buf_syself, 1, sz_syself, f_syself);
+
+        elf_sys.elfFile = buf_syself;
+        elf_sys.elfClass = ELFCLASS32;
+        elf_sys.elfSize = sz_syself;
+
+        exp_h = (exp_header_t *)buf_exp;
+
+        if (elf_checkFile(&elf_sys))
+        {
+            fprintf(stderr, "Error SysELF Format!\n");
+            goto exit02;
+        }
+
+        if (exp_h->Mark0 != 0x5AA52333 ||
+            exp_h->Mark1 != 1936291909 ||
+            exp_h->Mark2 != 1347436916)
+        {
+            fprintf(stderr, "Error EXP Format!\n");
+            goto exit02;
+        }
+        const char *sname;
+        size_t SysNumSection = elf_getNumSections(&elf_sys);
+        for (int i = 0; i < SysNumSection; i++)
+        {
+            sname = elf_getSectionName(&elf_sys, i);
+            if (strcmp(sname, ".symtab") == 0)
+            {
+                sys_symtab = (Elf32_Sym *)elf_getSectionOffset(&elf_sys, i);
+                syssym_num = elf_getSectionSize(&elf_sys, i) / sizeof(Elf32_Sym);
+                printf("sys_symtab:%08x, item:%d\n", sys_symtab, syssym_num);
+                sys_symtab = (Elf32_Sym *)((uint32_t)sys_symtab + (uint32_t)buf_syself);
+            }
+            if (strcmp(sname, ".strtab") == 0)
+            {
+                sys_symstr = (char *)elf_getSectionOffset(&elf_sys, i);
+                printf("sys_symstr:%08x\n", sys_symstr);
+                sys_symstr += (uint32_t)buf_syself;
+            }
+            if (strcmp(sname, ".sysinfo") == 0)
+            {
+                sysbuild_date = (char *)elf_getSectionOffset(&elf_sys, i);
+                printf("sysinfo:%08x\n", sysbuild_date);
+                sysbuild_date += (uint32_t)buf_syself;
+                printf("sysbuid date:%s\n", sysbuild_date);
+            }
+        }
+        syssym_hash = calc_sys_sym_hash();
+        printf("sys hash:%08x == %08x\n", syssym_hash, exp_h->sys_hash);
+        if (syssym_hash == exp_h->sys_hash)
+        {
+            printf("Hash is same, just copy date.\n");
+            strcpy(&exp_h->sys_build_date[0], sysbuild_date);
+            fseek(f_exp, 0, SEEK_SET);
+            fwrite(buf_exp, 1, sz_exp, f_exp);
+            goto exit02;
+        }
+        printf("resolve rel.dyn: \n\n");
+        // resolve rel.dyn
+        uint32_t *fill_addr;
+        Elf32_Sym *sym;
+        Elf32_Rel *rel_d = (Elf32_Rel *)(buf_exp + (uint32_t)exp_h->rel_dyn_of);
+
+        for (int i = 0; i < exp_h->num_rel_dyn; i++)
+        {
+            sym = (Elf32_Sym *)((uint32_t)buf_exp + (uint32_t)exp_h->dynsym_of);
+            sym += (uint32_t)(rel_d[i].r_info >> 8);
+            // printf("sym:%08x\n", sym->st_value);
+
+            if ((rel_d[i].r_info & 0xFF) == R_ARM_RELATIVE)
+            {
+                continue;
+            }
+            else if ((rel_d[i].r_info & 0xFF) == R_ARM_GLOB_DAT)
+            {
+                // printf("GLOB_DAT, at:%08x, link_to_addr:%08x, sym:%s \n",rel_d[i].r_offset, sym->st_value, &dynstr[sym->st_name]  );
+
+                INFO("GLOB_DAT, at:%08x, link_to_addr:%08x, ", rel_d[i].r_offset, sym->st_value);
+                if ((rel_d[i].r_offset >= exp_h->data_load_addr) && (rel_d[i].r_offset < exp_h->data_load_addr + exp_h->data_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->data_load_addr + exp_h->data_fo]));
+                    INFO("content(in DATA): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->reloc_load_addr) && (rel_d[i].r_offset < exp_h->reloc_load_addr + exp_h->reloc_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->reloc_load_addr + exp_h->reloc_fo]));
+                    INFO("content(in REL ): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->text_load_addr) && (rel_d[i].r_offset < exp_h->text_load_addr + exp_h->text_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->text_load_addr + exp_h->text_fo]));
+                    INFO("content(in TEXT): %08x ", *fill_addr);
+                }
+                else
+                {
+                    fprintf(stderr, "ERROR 2: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                    ret = -1;
+                    goto exit02;
+                }
+
+                INFO("sym :%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+            }
+            else if ((rel_d[i].r_info & 0xFF) == R_ARM_ABS32)
+            {
+
+                INFO("R_ARM_ABS32, at:%08x, link_to_addr:%08x, ", rel_d[i].r_offset, sym->st_value);
+                if ((rel_d[i].r_offset >= exp_h->data_load_addr) && (rel_d[i].r_offset < exp_h->data_load_addr + exp_h->data_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->data_load_addr + exp_h->data_fo]));
+                    INFO("content(in DATA): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->reloc_load_addr) && (rel_d[i].r_offset < exp_h->reloc_load_addr + exp_h->reloc_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->reloc_load_addr + exp_h->reloc_fo]));
+                    INFO("content(in REL ): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->text_load_addr) && (rel_d[i].r_offset < exp_h->text_load_addr + exp_h->text_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->text_load_addr + exp_h->text_fo]));
+                    INFO("content(in TEXT): %08x ", *fill_addr);
+                }
+                else
+                {
+                    fprintf(stderr, "ERROR 2: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                    ret = -1;
+                    goto exit02;
+                }
+
+                INFO("sym :%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+            }
+            else if ((rel_d[i].r_info & 0xFF) == R_ARM_JUMP_SLOT)
+            {
+
+                INFO("R_ARM_JUMP_SLOT, at:%08x, link_to_addr:%08x, ", rel_d[i].r_offset, sym->st_value);
+                if ((rel_d[i].r_offset >= exp_h->data_load_addr) && (rel_d[i].r_offset < exp_h->data_load_addr + exp_h->data_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->data_load_addr + exp_h->data_fo]));
+                    INFO("content(in DATA): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->reloc_load_addr) && (rel_d[i].r_offset < exp_h->reloc_load_addr + exp_h->reloc_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->reloc_load_addr + exp_h->reloc_fo]));
+                    INFO("content(in REL ): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->text_load_addr) && (rel_d[i].r_offset < exp_h->text_load_addr + exp_h->text_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->text_load_addr + exp_h->text_fo]));
+                    INFO("content(in TEXT): %08x ", *fill_addr);
+                }
+                else
+                {
+                    fprintf(stderr, "ERROR 2: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                    ret = -1;
+                    goto exit02;
+                }
+
+                INFO("sym :%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+            }
+            else
+            {
+
+                fprintf(stderr, "ERROR 2: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                ret = -1;
+                goto exit02;
+            }
+
+            if (sym->st_value == 0)
+            {
+                uint32_t sym_in_sys = search_sys_sym((char *)&buf_exp[sym->st_name + exp_h->dynstr_of]);
+                if (sym_in_sys == 0xFFFFFFFF)
+                {
+                    fprintf(stderr, "ERROR: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                    ret = -1;
+                    goto exit2;
+                }
+                *fill_addr = sym_in_sys;
+                printf("Relocate, at:%08x, link_addr-app:%08x -> new sys:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &buf_exp[sym->st_name + exp_h->dynstr_of]);
+            }
+        }
+
+        // resolve rel.plt
+        printf("resolve rel.plt: \n\n");
+
+        rel_d = (Elf32_Rel *)(buf_exp + (uint32_t)exp_h->rel_plt_of);
+        for (int i = 0; i < exp_h->num_rel_plt; i++)
+        {
+            sym = (Elf32_Sym *)((uint32_t)buf_exp + (uint32_t)exp_h->dynsym_of);
+            sym += (uint32_t)(rel_d[i].r_info >> 8);
+
+            if ((rel_d[i].r_info & 0xFF) == R_ARM_JUMP_SLOT)
+            {
+
+                INFO("R_ARM_JUMP_SLOT, at:%08x, link_to_addr:%08x, ", rel_d[i].r_offset, sym->st_value);
+                if ((rel_d[i].r_offset >= exp_h->data_load_addr) && (rel_d[i].r_offset < exp_h->data_load_addr + exp_h->data_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->data_load_addr + exp_h->data_fo]));
+                    INFO("content(in DATA): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->reloc_load_addr) && (rel_d[i].r_offset < exp_h->reloc_load_addr + exp_h->reloc_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->reloc_load_addr + exp_h->reloc_fo]));
+                    INFO("content(in REL ): %08x ", *fill_addr);
+                }
+                else if ((rel_d[i].r_offset >= exp_h->text_load_addr) && (rel_d[i].r_offset < exp_h->text_load_addr + exp_h->text_sz))
+                {
+                    fill_addr = ((uint32_t *)(&buf_exp[rel_d[i].r_offset - exp_h->text_load_addr + exp_h->text_fo]));
+                    INFO("content(in TEXT): %08x ", *fill_addr);
+                }
+                else
+                {
+                    fprintf(stderr, "ERROR 2: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                    ret = -1;
+                    goto exit02;
+                }
+                INFO("sym :%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+            }
+            else
+            {
+
+                fprintf(stderr, "ERROR 2: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                ret = -1;
+                goto exit02;
+            }
+            if (sym->st_value == 0)
+            {
+                uint32_t sym_in_sys = search_sys_sym((char *)&buf_exp[sym->st_name + exp_h->dynstr_of]);
+                if (sym_in_sys == 0xFFFFFFFF)
+                {
+                    fprintf(stderr, "ERROR: Unresolved Symbol:%s\n", &buf_exp[sym->st_name + exp_h->dynstr_of]);
+                    ret = -1;
+                    goto exit2;
+                }
+                *fill_addr = sym_in_sys;
+                printf("Relocate, at:%08x, link_addr-app:%08x -> new sys:%08x, sym: %s \n", rel_d[i].r_offset, sym->st_value, *fill_addr, &buf_exp[sym->st_name + exp_h->dynstr_of]);
+            }
+        }
+        exp_h->sys_hash = syssym_hash;
+        strcpy(&exp_h->sys_build_date[0], sysbuild_date);
+        fseek(f_exp, 0, SEEK_SET);
+        fwrite(buf_exp, 1, sz_exp, f_exp);
+        goto exit02;
     }
 
 exit2:
@@ -662,12 +975,19 @@ exit1:
     fclose(f_exp);
 
 exit0:
+    return ret;
 
+exit02:
+    free(buf_exp);
+    free(buf_syself);
+
+exit01:
+    fclose(f_syself);
+    fclose(f_exp);
+
+exit00:
     return ret;
 }
-
-
-
 void Usage()
 {
     // eld --appelf xxx.elf --syself xxx.elf --exp xxx.exp
@@ -677,4 +997,3 @@ void Usage()
     printf("\teld --syself xxx.elf --exp xxx.exp\n");
     exit(-1);
 }
-
